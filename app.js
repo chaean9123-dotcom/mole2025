a70712c97c2e64eb286783ba31d3bf1fe6479208
chaean9123-dotcom <232476167+chaean9123-dotcom@users.noreply.github.com>
2025-10-17 10:47:44 +0900

    up

---
+++ app.js
@@ -0,0 +1,47 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const score = document.querySelector("#score");
+const timeLeft = document.querySelector("#time-left");
+
+let timeId = null;
+let hitPosition;
+let result = 0;
+let currentTime = 10;
+
+function randomeSquae() {
+    squares.forEach((squares) => {
+        squares.classList.remove("mole");
+    });
+
+    let randomeSquae = squares[Math.floor(Math.random()*9)];
+    randomeSquae.classList.add("mole");
+    hitPosition = randomeSquae.id;
+}
+
+squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+        if(square.id == hitPosition) {
+            result++;
+            score.textContent = result;
+            hitPosition=null;
+        }
+    });
+});   
+
+function moveMole() {
+    timeId = setInterval(randomeSquae,500)
+}
+
+moveMole();
+
+function countDown() {
+    currentTime--;
+    timeLeft.textContent = currentTime;
+    if(currentTime == 0) {
+        clearInterval(countDownTimerId);
+        clearInterval(timeId);
+        alert("game over!" + result);
+    }
+}
+
+let countDownTimerId = setInterval(countDown,1000);
